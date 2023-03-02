d18b0eecb8879c6d9265fdb0c85ecceed24534cf
prtikakamble <pratikakamble085@gmail>
2023-03-02 15:46:12 +0530

    comment

--- crud/routes/web.php
+++ crud/routes/web.php
@@ -10,10 +10,12 @@ use App\Http\Controllers\StudentController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|Routes
 */
 
+
 Route::get('/', [StudentController::class, 'index'])->name('students');
+
 
 
 Route::post('/save-students/', [StudentController::class, 'saveStudents'])->name('savestudents');
